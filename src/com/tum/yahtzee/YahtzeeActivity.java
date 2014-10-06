c5d55f743b3479411cffa4c52ec659381c6e53db
Nico Alt <nicoalt@gmx.de>
2014-10-06 15:49:59 +0200

    changes in YahtzeeActivity: changed layout and cleaned code

--- src/com/tum/yahtzee/YahtzeeActivity.java
+++ src/com/tum/yahtzee/YahtzeeActivity.java
@@ -7,48 +7,31 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class YahtzeeActivity extends Activity {
 	
 	private EditText playerAmountText;
 	private EditText roundsText;
 	
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        TextView playerTitleView = (TextView)findViewById(R.id.player_title);
-        playerTitleView.setText("Amount of Players:");
-        
-        TextView roundTitle = (TextView)findViewById(R.id.game_rounds_title);
-        roundTitle.setText("Amount of Rounds:");
-        
-        playerAmountText = (EditText)findViewById(R.id.player_amount);
-        roundsText = (EditText)findViewById(R.id.game_rounds);
-        
-        Button playButton = (Button)findViewById(R.id.play_button);
-        playButton.setText("Play");
-        playButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				play();
-			}
-		});
+        playerAmountText = (EditText)findViewById(R.id.editText_player);
+        roundsText = (EditText)findViewById(R.id.editText_rounds);
     }
     
-    public void play()
+    public void play(View v)
     {
     	String sPlayers = playerAmountText.getText().toString();
     	String sRounds = roundsText.getText().toString();
     	
-    	if (sPlayers == null || sPlayers.isEmpty() || new Integer(sPlayers) == 0 || sRounds == null || sRounds.isEmpty() || new Integer(sRounds) == 0) {
+    	if (sPlayers == null || sPlayers.isEmpty() || Integer.parseInt(sPlayers) == 0 || sRounds == null || sRounds.isEmpty() || Integer.parseInt(sRounds) == 0) {
     		MessageService.showMessage(YahtzeeActivity.this, "Impossible", "Amount of Rounds and Players must not be zero.");
     	} else {
-	    	GameController.get().newGame(new Integer(sPlayers), new Integer(sRounds));
+	    	GameController.get().newGame(Integer.parseInt(sPlayers), Integer.parseInt(sRounds));
 	    	
 	    	Intent intent = new Intent(YahtzeeActivity.this, GameActivity.class);
 	    	startActivity(intent);
